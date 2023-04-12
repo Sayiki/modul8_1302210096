5550caabd4a4c63b06a3b7e4503e40d611033f2a
Sayiki <arzaqajradika1@gmail.com>
2023-04-12 17:23:18 +0700

    “aplikasi bank transfer dengan runtime configuration

---
+++ modul8_1302210096/BankTransferConfig.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+
+namespace modul8_1302210096
+{
+    internal class BankTransferConfig
+    {
+        public class config
+        {
+            public string lang { get; set; }
+            public transferr transfer { get; set; }
+            public List<string> methods { get; set; }
+            public konfirmasi confirmation { get; set; }
+
+
+
+            public class transferr
+            {
+                public int threshold;
+                public int low_fee;
+                public int high_fee;
+
+                public transferr(int threshold, int low_fee, int high_fee)
+                {
+                    this.threshold = threshold;
+                    this.low_fee = low_fee;
+                    this.high_fee = high_fee;
+                }
+
+                public transferr()
+                {
+
+                }
+            }
+
+            public class konfirmasi
+            {
+                public string en { get; set; }
+                public string id { get; set; }
+
+                public konfirmasi()
+                {
+
+                }
+
+                public konfirmasi(string en, string id)
+                {
+                    this.en = en;
+                    this.id = id;
+                }
+            }
+
+            public config(string lang, transferr transfer, List<string> methods,  konfirmasi confirmation)
+            {
+                this.lang = lang;
+                this.transfer = transfer;
+                this.methods = methods;
+                this.confirmation = confirmation;
+
+            }
+
+            public config()
+            {
+
+            }
+
+
+        }
+
+        public config ReadConfig()
+        {
+            string jsonFromFile = File.ReadAllText(path + '/' + configFileName);
+            Config = JsonSerializer.Deserialize<config>(jsonFromFile);
+
+
+            return Config;
+        }
+
+        public void WriteConfig()
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                WriteIndented = true,
+            };
+
+            String jsonString = JsonSerializer.Serialize(Config, options);
+            string fullPath = path + "/" + configFileName;
+            File.WriteAllText(fullPath, jsonString);
+        }
+
+
+        public config Config { get; set; }
+        public string configFileName = "bank_transfer_config.json";
+        public string path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+
+
+        
+
+        
+
+
+
+
+
+
+
+
+
+    }
+}
--- modul8_1302210096/Program.cs
+++ modul8_1302210096/Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+using modul8_1302210096;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        BankTransferConfig config = new BankTransferConfig();
+        config.ReadConfig();
+        if (config.Config.lang == "en")
+        {
+            Console.WriteLine("Please insert the amount of money to transfer: ");
+        }
+        else if ( config.Config.lang == "id")
+        {
+            Console.WriteLine("Masukkan jumlah uang yang akan di-transfer: ");
+        }
+
+        string amount = Console.ReadLine();
+
+
+
+
+    }
+}
